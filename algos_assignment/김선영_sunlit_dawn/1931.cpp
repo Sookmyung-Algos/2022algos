a08e69e382f16c58241af34aeb57a05e1f7fe6d0
sunlit02 <98438621+sunlit02@users.noreply.github.com>
2022-04-30 19:42:53 +0900

    Update 1931.cpp

--- algos_assignment/김선영_sunlit_dawn/1931.cpp
+++ algos_assignment/김선영_sunlit_dawn/1931.cpp
@@ -11,35 +11,36 @@ int c = 1;
 //오름차순 정렬
 bool compare(pair<int, int>v1, pair<int, int> v2) 
 {
-	if(v1.second == v2.second) 
-		return v1.first < v2.first;
-  else
-	  return v1.second < v2.second;
+    if(v1.second == v2.second) 
+	return v1.first < v2.first;
+    else
+        return v1.second < v2.second;
 }
 
 int main(void) 
 {
-	int start, end; 
-	int from, to; 
+    int start, end; 
+    int from, to; 
 	
-	cin >> n;
+    cin >> n;
 
-	for (int i = 0; i < n; i++) 
-  {
-		scanf("%d %d", &start, &end);
-		v.push_back(pair<int, int>(start, end));
-	}
+    for (int i = 0; i < n; i++) 
+    {
+	scanf("%d %d", &start, &end);
+	v.push_back(pair<int, int>(start, end));
+    }
 
-	sort(v.begin(), v.end(), compare); 
+    sort(v.begin(), v.end(), compare); 
 	
-	from = v[0].second; 
-	for(int i = 1; i < n; i++) 
-  {
-		to = v[i].first;
-		if(from <= to) {
-			c++;
-			from = v[i].second;
-		}
+    from = v[0].second; 
+    for(int i = 1; i < n; i++) 
+    {
+	to = v[i].first;
+	if(from <= to)
+	{
+	    c++;
+	    from = v[i].second;
 	}
-	cout << c;
+    }
+    cout << c;
 }
