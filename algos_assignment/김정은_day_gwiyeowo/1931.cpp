f7736ddeef10f67361ddc8fa7b64b7d220df4d85
epdlrnldudnj <102470872+epdlrnldudnj@users.noreply.github.com>
2022-05-01 01:47:12 +0900

    Create 1931.cpp

---
+++ algos_assignment/김정은_day_gwiyeowo/1931.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	vector<pair<int, int>> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i].second >> a[i].first;
+	}
+	sort(a.begin(), a.end());
+	int savetime = 0, ans = 0;
+	for (int i = 0; i < n; i++) {
+		if (savetime <= a[i].second) {
+			savetime = a[i].first;
+			ans++;
+		}
+	}
+	printf("%d", ans);
+
+}
+[출처] 백준 1931 회의실배정 풀이 C++|작성자 승원
