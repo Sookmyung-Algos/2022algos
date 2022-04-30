dca7b3e81dc32fddde1a7582a34d71d1d0b78356
jini-coding <96573160+jini-coding@users.noreply.github.com>
2022-05-01 02:55:23 +0900

    Update 1972.cpp

--- algos_assignment/권윤진_jini00/1972.cpp
+++ algos_assignment/권윤진_jini00/1972.cpp
@@ -1,1 +1,42 @@
-test
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    string str;
+    
+    while (true) {
+        cin >> str;
+        
+        if (str == "*")
+            break;
+        
+        bool check = true;
+        
+        for (int i = 1; i < str.length(); i++) {
+            set <string> v;
+            
+            for (int j = 0; j < str.size() - i; j++) {
+                string temp;
+                
+                temp += str[j];
+                temp += str[j+i];
+                
+                if (v.count(temp) == 0)
+                    v.insert(temp);
+                
+                else
+                    check = false;
+
+            }
+        }
+        
+        if (check == true)
+            cout << str << " is surprising." << "\n";
+        else
+            cout << str << " is NOT surprising." << "\n";
+            
+    }
+    return 0;
+}
