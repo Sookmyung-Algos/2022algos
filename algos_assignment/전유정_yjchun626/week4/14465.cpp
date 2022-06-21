77fc29525142dea3cefca8493028810cc337197f
yjchun626 <97106088+yjchun626@users.noreply.github.com>
2022-06-21 23:04:34 +0900

    Update 14465.cpp

--- algos_assignment/전유정_yjchun626/week4/14465.cpp
+++ algos_assignment/전유정_yjchun626/week4/14465.cpp
@@ -13,12 +13,10 @@ int main()
 	int now;
 	int ans = 100000;
     
- 	// map배열을 false값으로 초기화
 	memset(map, false, sizeof(map));
     
 	cin >> n >> k >> b;
 	
-	// b개의 고장난 신호등의 인덱스를 받아, map이라는 변수에 고장났다면 true로 갱신
 	for (int i = 1; i <= b; i++) 
 	{
 		cin >> now;
@@ -26,7 +24,7 @@ int main()
 	}
 	int cnt = 0;
     
-	// 1번 신호등부터 K번 신호등까지 몇개가 고장났는지 확인.
+	// 몇 개 고장났는지 개수 확인
 	for (int i = 1; i <= k; i++)
 		cnt += map[i];
 	ans = cnt;
@@ -34,7 +32,6 @@ int main()
 	int j = 1;
 	for (int i = k + 1; i <= n; i++)
 	{
-    	// 인덱스를 하나씩 밀어가며, 슬라이딩 윈도우의 가장 앞, 뒤만을 갱신하여 빠른 처리.
 		cnt += map[i];
 		cnt -= map[j++];
 		ans = min(ans, cnt);
