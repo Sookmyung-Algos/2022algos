7ff3b3deb9aa0812cdbd5b575c6a7ac5e0fdc766
SuminLim0 <81598640+SuminLim0@users.noreply.github.com>
2022-06-24 22:41:53 +0900

    Create 1135.cpp

---
+++ algos_assignment/임수민_jennylsm/4주차/1135.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> adj;
+int n;
+
+int dfs(int cur)
+{
+	vector<int> v;
+	int ret = 0;
+	int w = adj[cur].size() - 1;
+
+	for (int next : adj[cur])
+		v.push_back(dfs(next));
+	
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < v.size(); ++i)
+		ret = max(ret, v[i] + w--);
+		
+	return ret + 1;
+}
+
+int main()
+{
+	scanf("%d", &n);
+	adj.resize(n);
+
+	for (int i = 0; i < n; ++i) {
+		int p;
+		scanf("%d", &p);
+
+		if (p == -1)
+			continue;
+
+		adj[p].push_back(i);
+	}
+	int ans = dfs(0);
+
+	printf("%d", ans - 1);
+
+	return 0;
+}
