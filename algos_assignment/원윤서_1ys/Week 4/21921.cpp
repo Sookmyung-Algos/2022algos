a80782ea4b6a60994a74da6a05ad4626071a3746
hunt-and-peck <102416954+hunt-and-peck@users.noreply.github.com>
2022-06-26 03:26:36 +0900

    Update 21921.cpp

--- algos_assignment/원윤서_1ys/Week 4/21921.cpp
+++ algos_assignment/원윤서_1ys/Week 4/21921.cpp
@@ -6,34 +6,35 @@ using namespace std;
 long long sum[250001];
 
 int main() {
-  int n, m, num;
-  cin >> n >> m;
-  for(int i=0; i<n; i++) {
-    scanf("%d", &num);
-    if(i==0) {
-      sum[i] = num;
-      continue;
+    int n, x, num;
+    cin >> n >> x;
+    for (int i = 0; i < n; i++) {
+        scanf_s("%d", &num);
+        if (i == 0) {
+            sum[i] = num;
+            continue;
+        }
+        sum[i] = sum[i - 1] + num;
     }
-    sum[i] = sum[i-1] + num;
-  }
-  
-  int count = 1;
-  long long maxSum = sum[m-1];
-  
-  for(int i=m; i<n; i++) {
-    if(maxSum < sum[i] = sum[i-m]){
-      maxSum = sum[i] - sum[i-m];
-      count = 1;
+
+    int count = 1;
+    long long maxSum = sum[x - 1];
+
+    for (int i = x; i < n; i++) {
+        if (maxSum < sum[i] - sum[i - x]) {
+            maxSum = sum[i] - sum[i - x];
+            count = 1;
+        }
+        else if (maxSum == sum[i] - sum[i - x]) {
+            count += 1;
+        }
     }
-    else if(maxSum == sum[i] - sum[i-X]) {
-      count +=1;
+    if (maxSum == 0) {
+        printf("SAD");
     }
-  }
-  if(maxSum == 0) {
-    printf("SAD");
-  }
-  else {
-    printf("&lld\n", maxSum);
-    printf("%d", count);
-  }
+    else {
+        printf("%lld\n", maxSum);
+        printf("%d", count);
+    }
+    return 0;
 }
