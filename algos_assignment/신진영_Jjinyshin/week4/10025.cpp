c82c0edae848daeecc7acf007ea00dcb4c983428
Nyeong <87403267+Jjinyshin@users.noreply.github.com>
2022-06-25 03:09:34 -0700

    Add files via upload

---
+++ algos_assignment/신진영_Jjinyshin/week4/10025.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#define ll long long
+using namespace std;
+ll ice[1000002];
+ll d[1000002];
+int main() {
+    ll n, k, g, x;
+    ll ans = 0;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> g >> x;
+        ice[x] = g;
+    }
+    d[0] = ice[0];
+    for (int i = 1; i <= 1000000; i++)
+    {
+        d[i] = d[i - 1] + ice[i];
+    }
+    if (k <= 1000000)
+    {
+        for (int i = 0; i <= 1000000; i++)
+        {
+            if (i + k <= 1000000 && i - k >= 1)
+                ans = max(ans, d[i + k] - d[i - k - 1]);
+        }
+        cout << ans << '\n';
+    }
+    else
+        cout << d[1000000] << '\n';
+}
---
+++ algos_assignment/신진영_Jjinyshin/week4/14465.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N, K;
+vector<int> rion_position;
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int now;
+	cin >> N >> K;
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> now;
+		if (now == 1)
+			rion_position.push_back(i);
+	}
+
+	int ans = 1000001;
+
+	if (rion_position.size() < K)
+	{
+		cout << -1 << '\n';
+		return 0;
+	}
+
+	for (int i = 0; i <= rion_position.size() - K; i++)
+	{
+		ans = min(ans, rion_position[i + K - 1] - rion_position[i] + 1);
+	}
+
+	cout << ans << '\n';
+}
