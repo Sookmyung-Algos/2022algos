0f2bdc1eaf48871d08bf3fe18a9db2bf2f3db062
sobinMoon <102456745+sobinMoon@users.noreply.github.com>
2022-07-03 12:51:23 +0900

    Update 11724.cpp

--- algos_assignment/문소빈_mmtds6/5주차/11724.cpp
+++ algos_assignment/문소빈_mmtds6/5주차/11724.cpp
@@ -6,35 +6,28 @@ vector<int> vect[1001];
 int visited[1001];      
 int N, M;
 
-void DFS(int vertex)
-{
+void DFS(int vertex){
     visited[vertex] = 1;
-    for (int i = 0; i < vect[vertex].size(); i++) 
-    {
+    for (int i = 0; i < vect[vertex].size(); i++){
         int idx = vect[vertex][i];
-        if (visited[idx] == 0)
-        {
+        if (visited[idx] == 0){
             DFS(idx);
         }
     }
 }
 
-int main()
-{
+int main(){
     int u, v;
     int cnt = 0;
     cin >> N >> M;
-    for (int i = 0; i < M; i++)
-    {
+    for (int i = 0; i < M; i++){
         cin >> u >> v;
         vect[u].push_back(v);
         vect[v].push_back(u);
     }
 
-    for (int i = 1; i <= N; i++) 
-    {
-        if (visited[i] == 0)
-        {
+    for (int i = 1; i <= N; i++){
+        if (visited[i] == 0){
             cnt++;
             DFS(i);
         }
