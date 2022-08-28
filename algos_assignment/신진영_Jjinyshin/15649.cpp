46fcdd78645f6988847d9228f40e25fe2d0d6993
Nyeong <87403267+Jjinyshin@users.noreply.github.com>
2022-08-29 00:01:50 +0900

    Create 15649.cpp

---
+++ algos_assignment/신진영_Jjinyshin/15649.cpp
@@ -0,0 +1,29 @@
+# include <iostream>
+# define MAX 9
+using namespace std;
+
+int n,m;
+int arr[MAX] = {0,};
+bool visited[MAX] = {0,};
+
+void dfs(int count){
+    if (count == m){
+        for (int i = 0; i < m; i++){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = 1; i<= n; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            arr[count] = i;
+            dfs(count + 1);
+            visited[i] = false;
+        }
+    }
+}
+int main(){
+    cin >> n >> m;
+    dfs(0);
+}
