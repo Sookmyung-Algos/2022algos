74202bebae5cf928928dfba737fa9b2892e91d05
mzeong <70924556+mzeong@users.noreply.github.com>
2022-10-30 19:32:01 +0900

    Create 13317.cpp

---
+++ algos_assignment/Gold 1~3/벨만포드/김민정_mzeong/13317.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    
+    cout << 50 << " " << 49 << "\n";
+    for (int i = 49; i > 0; i--) cout << i << " " << i + 1 << " " << -1 << "\n";
+    return 0;
+}
